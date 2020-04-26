5eab12bceb2fe66d6a0c9bbaad78cbd9de0626a1
Jmack66 <33293161+Jmack66@users.noreply.github.com>
2020-04-26 14:54:17 +0100

    made a simulation class

--- rocket_test_v2.1.py
+++ rocket_test_v2.1.py
@@ -4,64 +4,61 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import time
 from matplotlib import style
-timestep = 0.05
+timestep = 0.05 #sets how quickly the simulation refreshes (0.05 is a happy medium)
 MASS = 1 #kg
 MAX_THRUST = 15 #Newtons
 g = -9.81 #Gravitational constant
 V_i = 0 #initial velocity 
 Y_i = 0 #initial height
 #--------PID GAINS--------
-kp = 0.25 
-ki = 0.4	
-kd = 0.05	
+kp = 0.2
+ki = 0	
+kd = 0
+#-------------------------
 SETPOINT = 10
+
 def main():
-	initScreen()
-	initMarker(SETPOINT)
-	initRocket()
-	sim = True
-	r = Rocket()
-	pid = PID(kp,ki,kd,SETPOINT)
-	ddy = 0
-	dy = 0
-	y = 0
-	while sim:
-		thrust = pid.compute(y)
-		r.set_ddy(thrust)
-		ddy = r.get_ddy()
-		dy = r.get_dy()
-		y = rocket.ycor()
-		rocket.sety(y + dy)
-		time.sleep(timestep)
-		if y > 800:
-			sim = False
-		elif y < -800:
-			sim = False
+	sim1 = Simulation()
+	sim1.cycle()
+
+class Simulation:
+	def __init__(self):
+		self.screen = turtle.Screen()
+		self.screen.setup(1200,800)
+		self.marker = turtle.Turtle()
+		self.marker.penup()
+		self.marker.left(180)
+		self.marker.goto(15,SETPOINT)
+		self.marker.color('red')
+		self.sim = True
+		self.timer = 0
+	def cycle(self):
+		r = Rocket()
+		pid = PID(kp,ki,kd,SETPOINT)
+		while self.sim:
+			thrust = pid.compute(r.get_y())
+			r.set_ddy(thrust)
+			r.set_dy()
+			r.set_y()
+			time.sleep(timestep)
+			self.timer += timestep
+			print(r.get_y(),thrust)
+			if r.get_y() > 800:
+			 	self.sim = False
+			elif r.get_y() < -800:
+				self.sim = False
+		
 
 
-def initMarker(SETPOINT):
-	global marker
-	marker = turtle.Turtle()
-	marker.penup()
-	marker.left(180)
-	marker.goto(15, SETPOINT)
-	marker.color("red")
-
-def initRocket():
-	global rocket
-	rocket = turtle.Turtle()
-	rocket.shape('square')
-	marker.penup()
-	rocket.penup()
-	rocket.speed(0)
-	rocket.goto(0,-100)
-def initScreen():
-	global screen
-	screen = turtle.Screen()
-	screen.setup(800,800)
-	
+		
 class Rocket:
 	def __init__(self):
+		global rocket
+		rocket = turtle.Turtle()
+		rocket.shape('square')
+		rocket.penup()
+		rocket.speed(0)
+		rocket.goto(0,-100)
 		self.ddy = 0
 		self.dy = V_i
 		self.y = Y_i
@@ -69,9 +66,15 @@ class Rocket:
 		self.ddy = g + thrust / MASS
 	def get_ddy(self):
 		return self.ddy
-	def get_dy(self):
+	def set_dy(self):
 		self.dy += self.ddy
-		return  self.dy
+	def get_dy(self):
+		return self.dy
+	def set_y(self):
+		rocket.sety(self.y + self.dy)
+	def get_y(self):
+		self.y = rocket.ycor()
+		return self.y
 
 class PID:
 	def __init__(self,kp,ki,kd,SETPOINT):
