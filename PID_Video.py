caae0159b6ded7558bc038b22e9acc0afd8777d9
kesmtk <45457917+kesmtk@users.noreply.github.com>
2020-09-25 23:36:44 +0300

    Update PID_Video.py
    Added additional graphs

--- PID_Video.py
+++ PID_Video.py
@@ -7,7 +7,7 @@ import time
 TIMER = 0
 TIME_STEP = 0.001
 SETPOINT = 10
-SIM_TIME = 500
+SIM_TIME = 100
 INITIAL_X = 0
 INITIAL_Y = -100
 MASS = 1 #kg
@@ -22,6 +22,7 @@ Y_i = 0 #initial height
 KP = 0.36
 KI = 40.0
 KD = 0.0008099999999999997
+#KD = 0.00128 for higher setpoints
 antiWindup = True
 # KP = 0.6
 # KI = 0.0
@@ -33,7 +34,7 @@ class Simulation(object):
 		self.Insight = Rocket()
 		self.pid = PID(KP,KI,KD,SETPOINT)
 		self.screen = turtle.Screen()
-		self.screen.setup(1280,900)
+		self.screen.setup(800,600)
 		self.marker = turtle.Turtle()
 		self.marker.penup()
 		self.marker.left(180)
@@ -43,6 +44,10 @@ class Simulation(object):
 		self.timer = 0
 		self.poses = np.array([])
 		self.times = np.array([])
+		self.kpe = np.array([])
+		self.kde = np.array([])
+		self.kie = np.array([])
+		self.thrst = np.array([])
 	def cycle(self):
 		while(self.sim):
 			thrust = self.pid.compute(self.Insight.get_y())
@@ -55,20 +60,34 @@ class Simulation(object):
 			if self.timer > SIM_TIME:
 				print("SIM ENDED")
 				self.sim = False
-			elif self.Insight.get_y() > 800:
+			elif self.Insight.get_y() > 700:
 				print("OUT OF BOUNDS")
 				self.sim = False
-			elif self.Insight.get_y() < -800:
+			elif self.Insight.get_y() < -700:
 				print("OUT OF BOUNDS")
 				self.sim = False
 			self.poses = np.append(self.poses,self.Insight.get_y())
 			self.times = np.append(self.times,self.timer)
-		graph(self.times,self.poses)
+			self.kpe = np.append(self.kpe,self.pid.get_kpe())
+			self.kde = np.append(self.kde,self.pid.get_kde())
+			self.kie = np.append(self.kie,self.pid.get_kie())
+			self.thrst = np.append(self.thrst,thrust)
+		graph(self.times,self.poses,self.kpe,self.kde,self.kie,self.thrst)
 
-def graph(x,y):
-	plt.plot(x,y)
+def graph(x,y1,y2,y3,y4,y5):
+	fig, (ax1, ax2,ax3,ax4,ax5) = plt.subplots(5, sharex=True)
+	#fig.suptitle('antiwindup')
+	ax1.set(ylabel='rocket \nHeight')
+	ax1.plot(x,y1)
+	ax2.set(ylabel='KP_error')
+	ax2.plot(x,y2,'tab:red')
+	ax3.set(ylabel='KD_error')
+	ax3.plot(x,y3,'tab:orange')
+	ax4.set(ylabel='KI_error')
+	ax4.plot(x,y4,'tab:pink')
+	ax5.set(ylabel='rocket \nThrust')
+	ax5.plot(x,y5,'tab:brown')
 	plt.show()
-
 
 class Rocket(object):
 	def __init__(self):
@@ -129,6 +148,13 @@ class PID(object):
 		elif self.output <= 0:
 			self.output = 0
 		return self.output
+		
+	def get_kpe(self):
+		return self.kp*self.error
+	def get_kde(self):
+		return self.kd*self.derivative_error
+	def get_kie(self):
+		return self.ki*self.integral_error
 
 def main():
 	sim = Simulation()
